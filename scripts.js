2941633762c1303583b7ec0ad79ae4ea18411322
MouraGabriel53 <contato.gabrielnmoura@gmail.com>
2025-02-12 08:49:35 -0300

    fix dark and light mode bugs

--- scripts.js
+++ scripts.js
@@ -2,11 +2,14 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("dark")
 
-  const img = document.querySelector("#profile-img")
+  const imgProfile = document.querySelector("#profile-img")
+  const iconMode = document.querySelector("#iconMode")
 
   if (html.classList.contains("dark")) {
-    img.setAttribute("src", "./assets/logo-light.png")
+    imgProfile.setAttribute("src", "./assets/logo-light.png")
+    iconMode.setAttribute("src", "./assets/night-icon.svg")
   } else {
-    img.setAttribute("src", "./assets/logo-dark.png")
+    imgProfile.setAttribute("src", "./assets/logo-dark.png")
+    iconMode.setAttribute("src", "./assets/day-icon.svg")
   }
 }
