07917064af8341fb43deeaf87f0286db69c8a7f6
Gabriel Moura <162451458+MouraGabriel53@users.noreply.github.com>
2025-02-25 16:37:57 -0300

    feat: update general folders structure

--- scripts.js
+++ scripts.js
@@ -7,13 +7,13 @@ function toggleMode() {
   const imgPython = document.querySelector("#imgPython")
 
   if (html.classList.contains("dark")) {
-    imgProfile.setAttribute("src", "./assets/logo-light.png")
-    iconMode.setAttribute("src", "./assets/night-icon.svg")
-    imgPython.setAttribute("src", "./assets/python-dark.svg")
+    imgProfile.setAttribute("src", "./assets/icons/logo-light.png")
+    iconMode.setAttribute("src", "./assets/icons/night-icon.svg")
+    imgPython.setAttribute("src", "./assets/logo-languages/python-dark.svg")
   } else {
-    imgProfile.setAttribute("src", "./assets/logo-dark.png")
-    iconMode.setAttribute("src", "./assets/day-icon.svg")
-    imgPython.setAttribute("src", "./assets/python-light.svg")
+    imgProfile.setAttribute("src", "./assets/icons/logo-dark.png")
+    iconMode.setAttribute("src", "./assets/icons/day-icon.svg")
+    imgPython.setAttribute("src", "./assets/logo-languages/python-light.svg")
   }
 }
 
@@ -25,10 +25,10 @@ function toggleLanguage() {
   const selectedLanguage = document.querySelector("#selectedLanguage")
 
   if (html.classList.contains("language-EN")) {
-    imgLanguage.setAttribute("src", "./assets/eua.png")
+    imgLanguage.setAttribute("src", "./assets/icons/eua.png")
     selectedLanguage.innerHTML = "EN"
   } else {
-    imgLanguage.setAttribute("src", "./assets/br.png")
+    imgLanguage.setAttribute("src", "./assets/icons/br.png")
     selectedLanguage.innerHTML = "PT"
   }
 }
