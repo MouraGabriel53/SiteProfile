6927c9e029bc4a0eba09af3a727347686ce80757
Gabriel Moura <162451458+MouraGabriel53@users.noreply.github.com>
2025-02-28 11:39:46 -0300

    :bug: bug: css and js logo doesn't appear on light mode

--- scripts.js
+++ scripts.js
@@ -4,28 +4,13 @@ function toggleMode() {
 
   const imgProfile = document.querySelector("#imgProfile")
   const iconMode = document.querySelector("#iconMode")
-  const imgPython = document.querySelector("#imgPython")
-  const imgCpp = document.querySelector("#imgCpp")
-  const imgHtml = document.querySelector("#imgHtml")
-  const imgCss = document.querySelector("#imgCss")
-  const imgJs = document.querySelector("#imgJs")
 
   if (html.classList.contains("light")) {
-    imgProfile.setAttribute("src", "../assets/icons/logo-light.png")
-    iconMode.setAttribute("src", "../assets/icons/day-icon.svg")
-    imgPython.setAttribute("src", "../assets/logo-languages/python-light.svg")
-    imgCpp.setAttribute("src", "../assets/logo-languages/cpp-light.svg")
-    imgHtml.setAttribute("src", "../assets/logo-languages/html-light.svg")
-    imgCss.setAttribute("src", "../assets/logo-languages/css-light.svg")
-    imgJs.setAttribute("src", "../assets/logo-languages/js-light.svg")
+    imgProfile.setAttribute("src", "./assets/icons/logo-light.png")
+    iconMode.setAttribute("src", "./assets/icons/day-icon.svg")
   } else {
-    imgProfile.setAttribute("src", "../assets/icons/logo-dark.png")
-    iconMode.setAttribute("src", "../assets/icons/night-icon.svg")
-    imgPython.setAttribute("src", "../assets/logo-languages/python-dark.svg")
-    imgCpp.setAttribute("src", "../assets/logo-languages/cpp-dark.svg")
-    imgHtml.setAttribute("src", "../assets/logo-languages/html-dark.svg")
-    imgCss.setAttribute("src", "../assets/logo-languages/css-dark.svg")
-    imgJs.setAttribute("src", "../assets/logo-languages/js-dark.svg")
+    imgProfile.setAttribute("src", "./assets/icons/logo-dark.png")
+    iconMode.setAttribute("src", "./assets/icons/night-icon.svg")
   }
 }
 
