80b4f0bb5624a26c3b8e777e5f64b09e1f3cd2b9
MouraGabriel53 <contato.gabrielnmoura@gmail.com>
2025-03-10 16:32:40 -0300

    :sparkles: feat: now available in english and portuguese | fixed

---
+++ scripts/toggleMode.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const imgProfile = document.querySelector("#imgProfile")
+  const iconMode = document.querySelector("#iconMode")
+
+  html.classList.contains("light")
+    ? (imgProfile.setAttribute("src", "./assets/icons/logo-light.png"),
+      iconMode.setAttribute("src", "./assets/icons/day-icon.svg"))
+    : (imgProfile.setAttribute("src", "./assets/icons/logo-dark.png"),
+      iconMode.setAttribute("src", "./assets/icons/night-icon.svg"))
+}
