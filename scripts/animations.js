d56f79af39a8a2eed43d3961b06e4458da36fc81
MouraGabriel53 <contato.gabrielnmoura@gmail.com>
2025-03-20 12:13:17 -0300

    feat: fix last bug

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,12 +1,21 @@
 const myObserve = new IntersectionObserver((isVisible) => {
   isVisible.forEach((element) => {
+    const elementScroll = document.getElementById("skills-container")
     if (element.isIntersecting === true) {
       element.target.classList.add("animation-show")
+      elementScroll.style.animationPlayState = "paused"
     } else {
       element.target.classList.remove("animation-show")
+      elementScroll.style.animationPlayState = "paused"
     }
   })
 })
 
 const elements = document.querySelectorAll(".animation-hidden")
 elements.forEach((element) => myObserve.observe(element))
+
+const elementBlinking = document.getElementById("headerCoverTrans")
+setTimeout(() => {
+  elementBlinking.style.animation = "none"
+  elementBlinking.style.borderRight = "none"
+}, 2000)
